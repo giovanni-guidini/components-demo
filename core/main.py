fcdd7d79a15f4828081c0bcf074ead355af3887f
Gguidini <giovanni@codecov.io>
2023-02-13 00:06:45 -0300

    add / remove stuff to throw off shift by lines

--- core/main.py
+++ core/main.py
@@ -1,58 +1,50 @@
 
-#comment comment
-#comment comment
+#comments comments
+#comments comments
 from os import environ
 from emotions.ascii import AsciiEmotions
 from emotions.emoji import EmojiEmotions
 
-#comment comment
-#comment comment
+#comments comments
 
 from fruits.ascii import AsciiFruit
 from fruits.emoji import EmojiFruit
 
-#comment comment
-#comment comment
-#comment comment
-#comment comment
+#comments comments
+#comments comments
 
 def get_emotion_class():
-    #comment comment
-    #comment comment
     emoji_support = environ.get('SUPPORTS_EMOJI', 'no')
-    print(emoji_support)
+    print(f"DEBUG {emoji_support}")
     if emoji_support.lower() == 'yes' or emoji_support.lower() == 'true':
         return EmojiEmotions()
     return AsciiEmotions()
 
 def get_fruit_class():
     fruit_support = environ.get('SUPPORTS_EMOJI', 'no')
-    print(fruit_support)
+    print(f"DEBUG {fruit_support}")
     if fruit_support.lower() == 'yes' or fruit_support.lower() == 'true':
         return EmojiFruit()
     return AsciiFruit()
 
 def int_to_emotion(n: int):
-    #comment comment
-    #comment comment
+    #comments comments
+    #comments comments
     emotion_class = get_emotion_class()
     if n <= 0:
         return emotion_class.crying()
     elif n < 10:
         return emotion_class.frown()
-    #comment comment
-    #comment comment
     elif n < 50:
         return emotion_class.bored()
     return emotion_class.smile()
 
+def some_other_function():
+    print("Hello from other function")
+    return "HELLO"
+
 #comment comment
 #comment comment
-#comment comment
-#comment comment
-#comment comment
-#comment comment
-#comment comment
-#comment comment
-#comment comment
-#comment comment
+#comments comments
+#comments comments
+#comments comments

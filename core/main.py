5e0ec5ef29086f75715abe38f76a0f0ef67e17ac
Gguidini <giovanni@codecov.io>
2023-02-13 00:18:33 -0300

    mostly remove code / add code between covered lines

--- core/main.py
+++ core/main.py
@@ -1,50 +1,40 @@
 
-#comments comments
-#comments comments
 from os import environ
 from emotions.ascii import AsciiEmotions
 from emotions.emoji import EmojiEmotions
 
-#comments comments
-
 from fruits.ascii import AsciiFruit
 from fruits.emoji import EmojiFruit
 
-#comments comments
-#comments comments
 
 def get_emotion_class():
     emoji_support = environ.get('SUPPORTS_EMOJI', 'no')
-    print(f"DEBUG {emoji_support}")
+    print(f"DEBUG emoji_support={emoji_support}")
+    # comment
     if emoji_support.lower() == 'yes' or emoji_support.lower() == 'true':
+        # unnecessary comment
         return EmojiEmotions()
+    # unecessary comment
     return AsciiEmotions()
 
 def get_fruit_class():
     fruit_support = environ.get('SUPPORTS_EMOJI', 'no')
-    print(f"DEBUG {fruit_support}")
+    print(f"DEBUG fruit_support={fruit_support}")
     if fruit_support.lower() == 'yes' or fruit_support.lower() == 'true':
         return EmojiFruit()
     return AsciiFruit()
 
 def int_to_emotion(n: int):
-    #comments comments
-    #comments comments
+    print(f"DEBUG n={n}")
     emotion_class = get_emotion_class()
     if n <= 0:
+        print(f"DEBUG returning crying")
         return emotion_class.crying()
     elif n < 10:
+        print(f"DEBUG returning frown")
         return emotion_class.frown()
     elif n < 50:
+        print(f"DEBUG returning bored")
         return emotion_class.bored()
+    print(f"DEBUG returning smile")
     return emotion_class.smile()
-
-def some_other_function():
-    print("Hello from other function")
-    return "HELLO"
-
-#comment comment
-#comment comment
-#comments comments
-#comments comments
-#comments comments

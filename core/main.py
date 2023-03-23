081c60a008e03a646145f40a91370a7be870ba7b
Gguidini <giovanni@codecov.io>
2023-03-23 11:27:10 +0100

    Over engineer a simple menu for no good reason

--- core/main.py
+++ core/main.py
@@ -1,24 +1,30 @@
+from typing import List
+
 from core.helpers import get_emotion_class, get_fruit_class
 from core.input import parse_input
 
 
 class MainMenu(object):
-    main_menu_message = ["What are you interested in?", "1. Fruits", "2. Emotions"]
-
     def __init__(self) -> None:
         self.emotion_class = get_emotion_class()
         self.fruit_class = get_fruit_class()
+
+    menu_options_index = {"1": "Fruits", "2": "Emotions"}
+
+    @property
+    def _menu_options(self) -> List[str]:
+        return [f"{key}.{value}" for key, value in self.menu_options_index.items()]
 
     def run(self, *args, **kwargs):
         loop = True
         while loop:
             print("Welcome to the Gallery!\n")
-            print("\n".join(self.main_menu_message))
+            print("\n".join(["Available expos:"] + self._menu_options))
             menu_choice = parse_input(
-                "What are you interested in seeing?",
-                valid_input=[1, 2],
-                pre_process_input=int,
-                error_message="Invalid choice.\n" + "\n".join(self.main_menu_message),
+                "What are you interested in seeing? ",
+                valid_input=self.menu_options_index.keys(),
+                error_message="Invalid choice.\n"
+                + "\n".join(["Available expos:"] + self._menu_options),
             )
-            print(f"You selected {menu_choice}")
+            print(f"You selected the {self.menu_options_index[menu_choice]} expo.")
             loop = False
